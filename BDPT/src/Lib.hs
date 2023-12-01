475d6831e598a887e55ed5f387601f622390a102
Wesley Chang <31712902+wchang22@users.noreply.github.com>
2023-12-01 20:21:38 +0000

    Brick runs

--- BDPT/src/Lib.hs
+++ BDPT/src/Lib.hs
@@ -1,20 +1,76 @@
 module Lib
-    ( someFunc
+    ( bdptApp
     ) where
-import System.Random
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+{-# LANGUAGE OverloadedStrings #-}
 
--- A Haskell program that does path tracing
--- 1. Generate a random number
--- 2. Generate a random vector
--- 3. Generate a random color
--- 4. Generate a random point
--- 5. Generate a random ray
--- 6. Generate a random sphere
--- 7. Generate a random hitable list
--- 8. Generate a random camera
--- 9. Generate a random material
--- 10. Generate a random lambertian
--- 11. Generate a random metal
+import Brick
+import qualified Graphics.Vty as V
+
+data Tick = Tick
+
+-- | Named resources
+type Name = ()
+type Image = ()
+
+data Pixel = Red | Green | Blue
+
+initialState :: Image
+initialState = ()
+
+height :: Int
+height = 3
+width :: Int
+width = 3
+
+data Coord = V2 Int Int
+
+drawGrid :: Image -> Widget Name
+drawGrid img = vBox rows
+      where 
+       -- draw all the cells in the grid.
+       -- a cell represent  disk, peg or Empty.
+       rows =  [ hBox $ cellsInRow r | r <- [height-1,height-2..0]]
+       cellsInRow y = [drawCoord (V2 x y) | x <- [0..width-1]]
+       drawCoord = drawPixel . pixelAt 
+       pixelAt (V2 x y)
+         | x == 0 && y == 0 = Red
+         | otherwise = Green
+
+drawPixel :: Pixel -> Widget Name 
+drawPixel Red = withAttr (attrName "red") cw
+drawPixel Green = withAttr (attrName "green") cw
+drawPixel Blue = withAttr (attrName "blue") cw
+
+cw :: Widget Name
+cw = str "  "
+
+drawUI :: Image -> [Widget Name]
+drawUI im = [drawGrid im]
+
+handleEvent :: BrickEvent Name Tick -> EventM Name Image ()
+handleEvent (VtyEvent (V.EvKey V.KEsc        [])) = halt
+handleEvent _ = return ()
+
+attributeMap :: AttrMap
+attributeMap = attrMap
+  V.defAttr [
+    (attrName "red", fg V.red),
+    (attrName "green", fg V.green),
+    (attrName "blue", fg V.blue)
+  ]
+
+bdptApp :: IO ()
+bdptApp = do
+  -- Initialize Brick app with default settings
+  let app =  App {
+    appDraw         = drawUI
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent  = handleEvent
+  , appStartEvent   = pure ()
+  , appAttrMap      = const attributeMap
+  }
+
+  -- Run the Brick app
+  _ <- defaultMain app initialState
+  return ()
