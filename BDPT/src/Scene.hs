58c5fbbc2bf0d3d619f2f5f39992b8d84a86d413
yhesper <hyin2147483647@gmail.com>
2023-12-09 01:09:49 +0000

    intersection

--- BDPT/src/Scene.hs
+++ BDPT/src/Scene.hs
@@ -1,7 +1,5 @@
 module Scene
-    ( Vector2(..)
-    , Vector3(..)
-    , Ray(..)
+    ( Ray(..)
     , Camera(..)
     , Intersection(..)
     , Primitive(..)
@@ -13,34 +11,43 @@ module Scene
 
 {-# LANGUAGE OverloadedStrings #-}
 
-data Vector2 = Vector2 {
-    u :: Float,
-    v :: Float
-} deriving (Eq, Show)
+import Linear.V3
 
-data Vector3 = Vector3 {
-    x :: Float,
-    y :: Float,
-    z :: Float
-} deriving (Eq, Show)
+-- data Vector2 = Vector2 {
+--     u :: Float,
+--     v :: Float
+-- } deriving (Eq, Show)
+
+-- data Vector3 = Vector3 {
+--     x :: Float,
+--     y :: Float,
+--     z :: Float
+-- } deriving (Eq, Show)
+
+v3Div :: V3 Float -> Float -> V3 Float
+v3Div (V3 x y z) s = V3 (x / s) (y / s) (z / s)
+
+v3Times (V3 x y z) s = V3 (x * s) (y * s) (z * s)
+
+v3Dot (V3 x1 y1 z1) (V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
 
 data Ray = Ray {
-    origin :: Vector3,
-    direction :: Vector3
+    origin :: V3 Float,
+    direction :: V3 Float
 } deriving (Eq, Show)
 
 data Camera = Camera {
-    position :: Vector3,
-    look_at :: Vector3,
-    up :: Vector3,
+    position :: V3 Float,
+    look_at :: V3 Float,
+    up :: V3 Float,
     fov :: Float
 } deriving (Eq, Show)
 
 data Intersection = Intersection {
     t :: Float,
     prim_idx :: Int,
-    normal :: Vector3,
-    color :: Vector3
+    normal :: V3 Float,
+    color :: V3 Float
 } deriving (Eq, Show)
 
 
@@ -50,23 +57,55 @@ class Primitive a where
   area :: a -> Float
 
 data Sphere = Sphere {
-    center :: Vector3,
+    center :: V3 Float,
     radius :: Float,
-    sphere_color  :: Vector3
+    sphere_color  :: V3 Float
 } deriving (Eq, Show)
+
+instance Primitive Sphere where
+  intersect s r = 
+    let
+      l = (center s) - (origin r)
+      tca = l `v3Dot` (direction r)
+      d2 = (l `v3Dot` l) - (tca * tca)
+      r2 = (radius s) * (radius s)
+      is_intersecting = (d2 <= r2)      
+    in 
+      if is_intersecting then
+        let 
+          thc = sqrt(r2 - d2)
+          t0 = tca - thc
+          t1 = tca + thc
+        in  
+          Intersection {
+            t = min t0 t1,
+            prim_idx = 0,
+            normal = ((origin r) + ((direction r) `v3Times` (min t0 t1))) - (center s),
+            color = sphere_color s
+          }
+      else
+        Intersection {
+            t = -1,
+            prim_idx = 0,
+            normal = V3 0 0 0,
+            color = V3 0 0 0
+          }
+  volume s = 4/3 * pi * (radius s) * (radius s) * (radius s)
+  area s = 4 * pi * (radius s) * (radius s)
 
 data Triangle = Triangle {
-    v1 :: Vector3,
-    v2 :: Vector3,
-    v3 :: Vector3,
-    triangle_color :: Vector3
+    v1 :: V3 Float,
+    v2 :: V3 Float,
+    v3 :: V3 Float,
+    triangle_color :: V3 Float
 } deriving (Eq, Show)
 
+
 data Mesh = Mesh {
-    vertices :: [Vector3],
+    vertices :: [V3 Float],
     indices  :: [Int],
-    normals  :: [Vector3],
-    colors   :: [Vector3]
+    normals  :: [V3 Float],
+    colors   :: [V3 Float]
 } deriving (Eq, Show)
 
 data Scene = Scene {
