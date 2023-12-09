2573edcc10dc4fe25ce450a72e60f9b357f809af
Wesley Chang <31712902+wchang22@users.noreply.github.com>
2023-12-09 00:31:46 +0000

    Render arbitrary rbg value

--- BDPT/src/Lib.hs
+++ BDPT/src/Lib.hs
@@ -6,14 +6,14 @@ module Lib
 
 import Brick
 import qualified Graphics.Vty as V
+import Linear.V2
+import Linear.V3
 
 data Tick = Tick
 
 -- | Named resources
 type Name = ()
 type Image = ()
-
-data Pixel = Red | Green | Blue
 
 initialState :: Image
 initialState = ()
@@ -23,25 +23,22 @@ height = 25
 width :: Int
 width = 25
 
-data Coord = V2 Int Int
-
 drawGrid :: Image -> Widget Name
 drawGrid img = vBox rows
   where 
     rows =  [ hBox $ pixelsInRow r | r <- [height-1,height-2..0]]
-    pixelsInRow y = [drawCoord (V2 x y) | x <- [0..width-1]]
-    drawCoord = drawPixel . pixelAt 
+    pixelsInRow y = [drawPixel (pixelAt (V2 x y)) | x <- [0..width-1]]
     pixelAt (V2 x y)
-      | x == 0 && y == 0 = Red
-      | otherwise = Green
+      | x == 0 && y == 0 = V3 255 0 0
+      | otherwise = V3 0 255 0
 
-drawPixel :: Pixel -> Widget Name 
-drawPixel Red = withAttr (attrName "red") cw
-drawPixel Green = withAttr (attrName "green") cw
-drawPixel Blue = withAttr (attrName "blue") cw
-
-cw :: Widget Name
-cw = str "██"
+-- Take an rgb value and draw the pixel as a widget
+drawPixel :: V3 Int -> Widget Name
+drawPixel (V3 r g b) = withAttr (attrName (rgbToAttrName r' g' b')) (str "██")
+  where
+    r' = mapToMaxColorResolution r
+    g' = mapToMaxColorResolution g
+    b' = mapToMaxColorResolution b
 
 drawUI :: Image -> [Widget Name]
 drawUI im = [drawGrid im]
@@ -50,13 +47,33 @@ handleEvent :: BrickEvent Name Tick -> EventM Name Image ()
 handleEvent (VtyEvent (V.EvKey V.KEsc        [])) = halt
 handleEvent _ = return ()
 
+-- The attribute map cannot support 255^3 color combinations, so specify
+-- the maximum range of rgb values
+maxColorResolution :: Int
+maxColorResolution = 100
+
+-- Functions to map between 0..255 and 0..maxColorResolution
+mapTo255 :: Integral a => a -> Int
+mapTo255 x = round ((fromIntegral x :: Float) * (255.0 / (fromIntegral maxColorResolution :: Float)))
+
+mapToMaxColorResolution :: Integral a => a -> Int
+mapToMaxColorResolution x = round ((fromIntegral x :: Float) * ((fromIntegral maxColorResolution :: Float) / 255.0))
+
+rgbToAttrName :: Int -> Int -> Int -> String
+rgbToAttrName r g b = show r ++ "-" ++ show g ++ "-" ++ show b
+
+-- Generate attribute map for colors in form of r-g-b -> RGBColor r g b
+colorMap :: [(AttrName, V.Attr)]
+colorMap = do
+  r <- [0..maxColorResolution] :: [Int]
+  g <- [0..maxColorResolution] :: [Int]
+  b <- [0..maxColorResolution] :: [Int]
+  let attrColor = V.linearColor (mapTo255 r) (mapTo255 g) (mapTo255 b)
+  return (attrName (rgbToAttrName r g b), fg attrColor)
+
 attributeMap :: AttrMap
 attributeMap = attrMap
-  V.defAttr [
-    (attrName "red", fg V.red),
-    (attrName "green", fg V.green),
-    (attrName "blue", fg V.blue)
-  ]
+  V.defAttr colorMap
 
 bdptApp :: IO ()
 bdptApp = do
